86d8ae4ffd5a714d770fcdea7d3e3c7b57df83f2
albejawe <albejawehack@gmail.com>
2025-07-26 08:07:59 +0300

    Remove android platform folder from repo

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'APP.AWQATTRAVELAPP.APP',
+  appName: 'أوقات للسياحة والسفر',
+  webDir: 'dist'
+};
+
+export default config;
