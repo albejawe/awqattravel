f3174c44b67a7ac22ad9a8bb92ebd7d2309ba861
albejawe <33374195+albejawe@users.noreply.github.com>
2025-07-26 07:58:56 +0300

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,31 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.awqat.travel',
-  appName: 'Awqat Travel',
-  webDir: 'dist',
-  bundledWebRuntime: false,
-  server: {
-    url: 'https://9af09eac-6975-449e-8806-c678c1dfa4d5.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 3000,
-      launchAutoHide: true,
-      backgroundColor: "#fffbeb",
-      androidSplashResourceName: "splash",
-      androidScaleType: "CENTER_CROP",
-      showSpinner: true,
-      androidSpinnerStyle: "large",
-      iosSpinnerStyle: "small",
-      spinnerColor: "#999999",
-      splashFullScreen: true,
-      splashImmersive: true,
-      layoutName: "launch_screen",
-      useDialog: true,
-    },
-  },
+  appId: 'APP.AWQATTRAVELAPP.APP',
+  appName: 'أوقات للسياحة والسفر',
+  webDir: 'dist'
 };
 
 export default config;
